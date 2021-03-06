dcfb55dd54b646c3e3048c3a4e95e979e3e07198
Leonardo Maia <leuow95@gmail.com>
2021-03-06 00:28:51 -0300

    testing the operations count, find and any

--- src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
@@ -16,6 +16,12 @@ fun main() {
     println("---------------")
     salariosMaiorque2500.forEach { println(it)}
 
+    println("---------------")
+    println(salarios.count { it in 2000.0..5000.0})
+    println("---------------")
+    println(salarios.find { it in 2000.0..5000.0})
+    println("---------------")
+    println(salarios.any { it > 1000.0})
 
 
 
