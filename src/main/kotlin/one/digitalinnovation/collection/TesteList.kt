88ad798282e49c93e7e854345e365e72440ad7a1
Leonardo Maia <leuow95@gmail.com>
2021-03-07 00:57:55 -0300

    added atribute tipoContratacao to class Funcionario and printed it using groupBy

--- src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -15,11 +15,16 @@ fun main() {
     funcionarios
         .sortedBy{it.salario}
         .forEach{println(it)}
+    println("-------------------------")
+    funcionarios
+        .groupBy{it.tipoContratacao}
+        .forEach{println(it)}
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
