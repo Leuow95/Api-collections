05d26723f7c9ff9a01808c7d839a3cc18b397e3e
Leonardo Maia <leuow95@gmail.com>
2021-03-07 17:51:25 -0300

    created and implemented TesteMutavelMap

--- src/main/kotlin/one/digitalinnovation/collection/Repositorio.kt
+++ src/main/kotlin/one/digitalinnovation/collection/Repositorio.kt
@@ -1,6 +1,11 @@
 package one.digitalinnovation.collection
 
-class Repositorio {
+class Repositorio<T> {
     private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+        map[id] = value
+    }
+    fun findById(id: String) =  map[id]
 
 }
