a225a6fbb9d645c4ee3638269467490bc624c7c9
Leonardo Maia <leuow95@gmail.com>
2021-03-07 17:41:30 -0300

    created TesteColecoesMutaveis to implemente mutablesetOf and mutablelistOf

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteColecoesMutaveis.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach {println(it)}
+
+    println("--------- LIST ----------")
+    funcionarios.add(pedro)
+    funcionarios.forEach {println(it)}
+
+    println("-------------------")
+    funcionarios.remove(joao)
+    funcionarios.forEach {println(it)}
+
+    println("---------- SET ---------")
+    val funcionarioSet = mutableSetOf(joao)
+    //funcionarioSet.forEach {println(it)}
+
+    funcionarioSet.add(maria)
+    funcionarioSet.add(pedro)
+    funcionarioSet.forEach {println(it)}
+
+
+
+}
